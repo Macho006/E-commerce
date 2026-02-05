f9ea132a6fa80125c99d98805ed2686def226d13
Yusuf_52 <yusufxon5225@gmail.com>
2026-02-05 16:12:59 +0500

    yusuf/fix

--- components/category/page.tsx
+++ components/category/page.tsx
@@ -1,17 +1,7 @@
 "use client";
 
 import React, { useState } from 'react';
-import { Heart, BarChart2, X } from 'lucide-react';
 import ProductCard from '../productcard';
-import Iphone from '@/public/Iphone.svg';
-import laptop from '@/public/laptop.svg';
-import tablet from '@/public/tablet.svg';
-import watch from '@/public/watch.svg';
-import gamepad from '@/public/gamepad.svg';
-import naushniki from '@/public/naushnik.svg';
-import jbl from '@/public/jbl.svg';
-import iphone2 from '@/public/iphone2.svg';
-
 
 export default function CatalogPage() {
   const [selectedFilters, setSelectedFilters] = useState(['Дизайн', 'Портативность']);
