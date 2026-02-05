62ded25ce410038bf9cba325aba86c5ea8e28b68
Ibrohim121 <igofurjonov229@gmail.com>
2026-02-05 15:38:29 +0500

    Footer-Ibrohim

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import Hero from '@/components/shared/hero'
 import CategoriesCard from '@/components/home/categories-card'
+import ReviewsSection from '@/components/home/reviews-section';
 import { v4 as uuidv4 } from 'uuid';
-import { ProductCard } from "@/components/shared/ProductCard";
+
 import { Footer } from "@/components/shared/Footer";
 import Products from "@/components/product/NewArrivalsSection"
 
@@ -55,32 +56,7 @@ let categories = [
   },
 ]
 
-const products = [
-  {
-    title: "Review of outdated smartphones",
-    description: "Lorem ipsum dolor sit amet consectetur. Rhoncus risus a viverra enim nunc.",
-    imageSrc: "/images/phone.jpg",
-    link: "#",
-  },
-  {
-    title: "Review of desktop computers",
-    description: "Lorem ipsum dolor sit amet consectetur. Rhoncus risus a viverra enim nunc.",
-    imageSrc: "/images/desktop.jpg",
-    link: "#",
-  },
-  {
-    title: "Review of Marshall amplifiers",
-    description: "Lorem ipsum dolor sit amet consectetur. Rhoncus risus a viverra enim nunc.",
-    imageSrc: "/images/amp.jpg",
-    link: "#",
-  },
-  {
-    title: "Review of headphones",
-    description: "Lorem ipsum dolor sit amet consectetur. Rhoncus risus a viverra enim nunc.",
-    imageSrc: "/images/headphones.jpg",
-    link: "#",
-  },
-];
+
 
 
 export default function HomePage() {
@@ -98,22 +74,10 @@ export default function HomePage() {
           />
         ))}
       </section>
-      <main className="max-w-6xl mx-auto px-4 py-10">
-      <h1 className="text-2xl font-bold mb-6">Reviews</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-        {products.map((product, index) => (
-          <ProductCard key={index} {...product} />
-        ))}
-      </div>
-
-      <div className="mt-6 text-right">
-        <a href="#" className="text-blue-600 hover:underline">To all reviews &gt;</a>
-      </div>
-
-      <Footer />
-    </main>
 
       <Products />
+
+      <ReviewsSection />
     </div>
   )
 }
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,19 +1,18 @@
 import Link from 'next/link'
 import React from 'react'
 import Navbar from '@/components/shared/navbar'
+import { Footer } from '@/components/shared/Footer'
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Navbar />
-      
+
       <main>
         {children}
       </main>
 
-      <footer>
-        Footer
-      </footer>
+      <Footer />
     </>
   )
 }
